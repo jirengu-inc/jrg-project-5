ce4cd88f1b21aa5f90a4494df50ebaec8dcbe6f4
frankfang <frankfang1990@gmail.com>
2017-01-11 10:46:16 +0800

    add step-1

---
+++ step-1/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+    ]
+  }
+}
