8bc5a642d15171059f7422def88dea91bfa4596a
frankfang <frankfang1990@gmail.com>
2017-01-20 15:08:40 +0800

    add currentUser

--- step-4/app.js
+++ step-4/app.js
@@ -17,7 +17,8 @@ var app = new Vue({
       password: ''
     },
     newTodo: '',
-    todoList: []
+    todoList: [],
+    currentUser: null,
   },
   created: function(){
     // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
@@ -48,10 +49,24 @@ var app = new Vue({
       let user = new AV.User();
       user.setUsername(this.formData.username);
       user.setPassword(this.formData.password);
-      user.signUp().then(function (loginedUser) {
-        console.log(loginedUser);
-      }, function (error) {
+      user.signUp().then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, (error) => {
+        alert('注册失败')
       });
+    },
+    login: function () {
+      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, function (error) {
+        alert('登录失败')
+      });
+    },
+    getCurrentUser: function () {
+      let {id, createdAt, attributes: {username}} = AV.User.current()
+      // 上面这句话看不懂就得看 MDN 文档了
+      // 我的《ES 6 新特性列表》里面有链接：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      return {id, username, createdAt} // 看文档：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Object_initializer#ECMAScript_6%E6%96%B0%E6%A0%87%E8%AE%B0
     }
   }
 })                                                               
