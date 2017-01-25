985bcb6bebbedd508ab544728431c7b2fe22ef37
frankfang <frankfang1990@gmail.com>
2017-01-25 17:31:52 +0800

    根据 id 选择 save(create) 或者 update 1. save 成功后保存 id 2. 读取成功后保存 id 3. 如果有 id，那么就更新原有记录，而不是新建

--- step-5/app.js
+++ step-5/app.js
@@ -26,8 +26,11 @@ var app = new Vue({
     if(this.currentUser){
       var query = new AV.Query('AllTodos');
       query.find()
-        .then(function (todos) {
-          console.log(todos)
+        .then((todos) => {
+          let avAllTodos = todos[0] // 因为理论上 AllTodos 只有一个，所以我们取结果的第一项
+          let id = avAllTodos.id
+          this.todoList = JSON.parse(avAllTodos.attributes.content) // 为什么有个 attributes？因为我从控制台看到的
+          this.todoList.id = id // 为什么给 todoList 这个数组设置 id？因为数组也是对象啊
         }, function(error){
           console.error(error) 
         })
@@ -35,6 +38,15 @@ var app = new Vue({
 
   },
   methods: {
+    updateTodos: function(){
+      // 想要知道如何更新对象，先看文档 https://leancloud.cn/docs/leanstorage_guide-js.html#更新对象
+      let dataString = JSON.stringify(this.todoList) // JSON 在序列化这个有 id 的数组的时候，会得出怎样的结果？
+      let avTodos = AV.Object.createWithoutData('AllTodos', this.todoList.id)
+      avTodos.set('content', dataString)
+      avTodos.save().then(()=>{
+        console.log('更新成功')
+      })
+    },
     saveTodos: function(){
       let dataString = JSON.stringify(this.todoList)
       var AVTodos = AV.Object.extend('AllTodos');
@@ -45,11 +57,19 @@ var app = new Vue({
 
       avTodos.set('content', dataString);
       avTodos.setACL(acl) // 设置访问控制
-      avTodos.save().then(function (todo) {
+      avTodos.save().then((todo) =>{
+        this.todoList.id = todo.id  // 一定要记得把 id 挂到 this.todoList 上，否则下次就不会调用 updateTodos 了
         alert('保存成功');
       }, function (error) {
         alert('保存失败');
       });
+    },
+    saveOrUpdateTodos: function(){
+      if(this.todoList.id){
+        this.updateTodos()
+      }else{
+        this.saveTodos()
+      }
     },
     addTodo: function(){
       this.todoList.push({
@@ -58,12 +78,12 @@ var app = new Vue({
         done: false // 添加一个 done 属性
       })
       this.newTodo = ''
-      this.saveTodos()
+      this.saveOrUpdateTodos() // 不能用 saveTodos 了
     },
     removeTodo: function(todo){
       let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是 ES 5 新加的 API
       this.todoList.splice(index,1) // 不懂 splice？赶紧看 MDN 文档！
-      this.saveTodos()
+      this.saveOrUpdateTodos() // 不能用 saveTodos 了
     },
     signUp: function () {
       let user = new AV.User();
