b328239865b2ef20491674111e5fee77e71efecb
frankfang <frankfang1990@gmail.com>
2017-02-05 16:21:36 +0800

    add styles for topbar

--- resumer/src/components/Topbar.vue
+++ resumer/src/components/Topbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="topbar">
-    I am topbar
+    <div class="wrapper">
+      <span class="logo">Resumer</span>
+    </div>
   </div>
 </template>
 
@@ -14,6 +16,20 @@ export default {
   #topbar{
     background:#ffffff;
     box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+  }
+  #topbar>.wrapper{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0 auto;
     height:64px;
+  }
+  #topbar>.wrapper{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  #topbar .logo{
+    font-size:24px;
+    color:#000000;
   }
 </style>
