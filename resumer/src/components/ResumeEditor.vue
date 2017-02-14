e81e3420baf65a77664baf1a76fc1db2ec3f0bb3
frankfang <frankfang1990@gmail.com>
2017-02-14 11:05:50 +0800

    将 visibleItems 改为 config

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -2,17 +2,14 @@
   <div id="resumeEditor">
     <nav>
       <ol>
-        <li v-for="(item,index) in resume.visibleItems" 
-            :class="{active: item === selected}"
-            @click="selected = item"
-            >
+        <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
           {{index}}
         </li>
       </ol>
     </nav>
     <ol class="panels">
-      <li v-for="item in resume.visibleItems" v-show="item === selected">
-        {{resume[item]}}
+      <li v-for="item in resume.config" v-show="item.field === selected">
+        {{resume[item.field]}}
       </li>
     </ol>
   </div>
@@ -25,7 +22,14 @@
       return {
         selected: 'profile',
         resume: {
-          visibleItems: ['profile', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+          config: [
+            { field: 'profile', icon: 'id' },
+            { field: 'work history', icon: 'work' },
+            { field: 'education', icon: 'book' },
+            { field: 'projects', icon: 'heart' },
+            { field: 'awards', icon: 'cup' },
+            { field: 'contacts', icon: 'phone' },
+          ],
           profile: {
             name: '',
             city: '',
@@ -36,7 +40,6 @@
           projects: [],
           awards: [],
           contacts: [],
-          others: []
         }
       }
     }
