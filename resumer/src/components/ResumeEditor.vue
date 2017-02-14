0d1801e2d5cf0650e48438756443525fe6f700c1
frankfang <frankfang1990@gmail.com>
2017-02-14 10:57:34 +0800

    bio 重命名为 profile

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -23,10 +23,10 @@
     name: 'ResumeEditor',
     data() {
       return {
-        selected: 'bio',
+        selected: 'profile',
         resume: {
-          visibleItems: ['bio', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
-          bio: {
+          visibleItems: ['profile', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+          profile: {
             name: '',
             city: '',
             title: ''
