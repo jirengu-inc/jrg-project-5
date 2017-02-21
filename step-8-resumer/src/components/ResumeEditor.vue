90595f66a7356574cd507b0b6b0d7bf8fdf612d4
frankfang <frankfang1990@gmail.com>
2017-02-21 12:40:18 +0800

    将原有数据移动到 store 里

--- step-8-resumer/src/components/ResumeEditor.vue
+++ step-8-resumer/src/components/ResumeEditor.vue
@@ -36,49 +36,15 @@
 <script>
   export default {
     name: 'ResumeEditor',
-    data() {
-      return {
-        selected: 'profile',
-        resume: {
-          config: [
-            { field: 'profile', icon: 'id' },
-            { field: 'work history', icon: 'work' },
-            { field: 'education', icon: 'book' },
-            { field: 'projects', icon: 'heart' },
-            { field: 'awards', icon: 'cup' },
-            { field: 'contacts', icon: 'phone' },
-          ],
-          profile: {
-            name: '',
-            city: '',
-            title: ''
-          },
-          'work history': [
-            { company: 'AL', content: '我的第二份工作是' },
-            { company: 'TX', content: '我的第一份工作是' },
-          ],
-          education: [
-            { school: 'AL', content: '文字' },
-            { school: 'TX', content: '文字' },
-          ],
-          projects: [
-            { name: 'project A', content: '文字' },
-            { name: 'project B', content: '文字' },
-          ],
-          awards: [
-            { name: 'awards A', content: '文字' },
-            { name: 'awards B', content: '文字' },
-          ],
-          contacts: [
-            { contact: 'phone', content: '13812345678' },
-            { contact: 'qq', content: '12345678' },
-          ],
-        }
-      }
-    },
     computed: {
       count () {
         return this.$store.state.count
+      },
+      selected () {
+        return this.$store.state.selected
+      },
+      resume (){
+        return this.$store.state.resume
       }
     },
     methods: {
--- step-8-resumer/src/store/index.js
+++ step-8-resumer/src/store/index.js
@@ -5,7 +5,43 @@ Vue.use(Vuex) // 不写这句话浏览器控制台就会报错，于是我就写
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    selected: 'profile',
+    resume: {
+      config: [
+        { field: 'profile', icon: 'id' },
+        { field: 'work history', icon: 'work' },
+        { field: 'education', icon: 'book' },
+        { field: 'projects', icon: 'heart' },
+        { field: 'awards', icon: 'cup' },
+        { field: 'contacts', icon: 'phone' },
+      ],
+      profile: {
+        name: '',
+        city: '',
+        title: ''
+      },
+      'work history': [
+        { company: 'AL', content: '我的第二份工作是' },
+        { company: 'TX', content: '我的第一份工作是' },
+      ],
+      education: [
+        { school: 'AL', content: '文字' },
+        { school: 'TX', content: '文字' },
+      ],
+      projects: [
+        { name: 'project A', content: '文字' },
+        { name: 'project B', content: '文字' },
+      ],
+      awards: [
+        { name: 'awards A', content: '文字' },
+        { name: 'awards B', content: '文字' },
+      ],
+      contacts: [
+        { contact: 'phone', content: '13812345678' },
+        { contact: 'qq', content: '12345678' },
+      ],
+    }
   },
   mutations: {
     increment (state) {
