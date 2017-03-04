1a9f4c8018881ebd2723455c277d4f68147a8d42
frankfang <frankfang1990@gmail.com>
2017-03-04 17:06:11 +0800

    删除所有假数据

--- step-10-resumer/src/store/index.js
+++ step-10-resumer/src/store/index.js
@@ -22,38 +22,28 @@ export default new Vuex.Store({
         { field: 'contacts', icon: 'phone' },
       ],
       profile: {
-        name: '方某某',
-        city: '大城市铁岭',
-        title: '首席装逼师',
-        birthday: '1990-01-01'
+        name: '',
+        city: '',
+        title: '',
+        birthday: ''
       },
       workHistory: [
         {
-          company: '鸡飞狗跳公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
-我的主要工作如下:
-1. 完成既定产品需求。
-2. 修复 bug。`
+          company: '', content: ``
         },
-        { company: '狗急跳墙责任有限公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
-我的主要工作如下:
-1. 完成既定产品需求。
-2. 修复 bug` },
+        { company: '', content: `` },
       ],
       education: [
-        { school: '黄志诚警官大学', content: '本科' },
-        { school: '韩琛古惑仔高中'},
+        { school: '', content: '' },
       ],
       projects: [
-        { name: 'project A', content: '文字' },
-        { name: 'project B', content: '文字' },
+        { name: '', content: '' },
       ],
       awards: [
-        { name: '再来十瓶', content: '连续十次获得「再来一瓶」奖励' },
-        { name: '三好学生'},
+        { name: '', content: '' },
       ],
       contacts: [
-        { contact: 'phone', content: '13812345678' },
-        { contact: 'qq', content: '12345678' },
+        { contact: '', content: '' },
       ],
     }
   },
