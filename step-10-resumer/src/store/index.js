dd54fbe4baf9245127c0e6a8b549b21067786c57
frankfang <frankfang1990@gmail.com>
2017-03-04 20:24:02 +0800

    修改 resume

--- step-10-resumer/src/store/index.js
+++ step-10-resumer/src/store/index.js
@@ -14,34 +14,19 @@ export default new Vuex.Store({
     },
     resume: {
       config: [
-        { field: 'profile', icon: 'id' },
-        { field: 'workHistory', icon: 'work' },
-        { field: 'education', icon: 'book' },
-        { field: 'projects', icon: 'heart' },
-        { field: 'awards', icon: 'cup' },
-        { field: 'contacts', icon: 'phone' },
+        { field: 'profile', icon: 'id', keys: ['name','city', 'title', 'birthday']},
+        { field: 'workHistory', icon: 'work', type: 'array', keys: ['company', 'details'] },
+        { field: 'education', icon: 'book',type: 'array',  keys: ['school', 'details'] },
+        { field: 'projects', icon: 'heart',type: 'array',  keys: ['name', 'details']  },
+        { field: 'awards', icon: 'cup' ,type: 'array',  keys: ['name', 'details'] },
+        { field: 'contacts', icon: 'phone' ,type: 'array',  keys: ['contact', 'content'] },
       ],
-      profile: {
-        name: '',
-        city: '',
-        title: '',
-        birthday: ''
-      },
-      workHistory: [
-        { company: '', content: `` },
-      ],
-      education: [
-        { school: '', content: '' },
-      ],
-      projects: [
-        { name: '', content: '' },
-      ],
-      awards: [
-        { name: '', content: '' },
-      ],
-      contacts: [
-        { contact: '', content: '' },
-      ],
+      profile: { },
+      workHistory: [ ],
+      education: [ ],
+      projects: [ ],
+      awards: [ ],
+      contacts: [ ],
     }
   },
   mutations: {
