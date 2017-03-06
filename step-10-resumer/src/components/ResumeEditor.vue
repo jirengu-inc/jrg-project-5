02f8ae0550746203d3efcb9f6342cf3f808c9e82
frankfang <frankfang1990@gmail.com>
2017-03-06 20:55:37 +0800

    完成删除按钮的点击事件

--- step-10-resumer/src/components/ResumeEditor.vue
+++ step-10-resumer/src/components/ResumeEditor.vue
@@ -14,7 +14,7 @@
         <div v-if="item.type === 'array'">
           <h2>{{$t(`resume.${item.field}._`)}}</h2>
           <div class="subitem" v-for="(subitem, i) in resume[item.field]">
-            <button class="button remove small">删除</button>
+            <button class="button remove small" @click="removeResumeSubfield(item.field, i)">删除</button>
             <div class="resumeField" v-for="(value,key) in subitem">
               <label> {{$t(`resume.${item.field}.${key}`)}}</label>
               <input type="text" :value="value" @input="changeResumeField(`${item.field}.${i}.${key}`, $event.target.value)">
@@ -60,6 +60,9 @@
       },
       addResumeSubfield(field){
         this.$store.commit('addResumeSubfield', {field})
+      },
+      removeResumeSubfield(field, index){
+        this.$store.commit('removeResumeSubfield', {field, index})
       }
     }
   }
--- step-10-resumer/src/store/index.js
+++ step-10-resumer/src/store/index.js
@@ -60,6 +60,9 @@ export default new Vuex.Store({
       state.resumeConfig.filter((i)=>i.field === field)[0].keys.map((key)=>{
         Vue.set(empty, key, '')
       })
+    },
+    removeResumeSubfield(state, {field, index}){
+      state.resume[field].splice(index, 1)
     }
 
   }
